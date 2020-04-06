9b78e73d3bad6f2b3f40f213a42ce15307548654
walter <santi.walter@gmail.com>
2020-04-06 16:34:59 +0200

    start

---
+++ Block/CustomerGroups.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ws\Regformcustomergroup\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Customer\Model\ResourceModel\Group\Collection as CustomerGroup;
+
+Class CustomerGroups extends Template {
+    public $_customerGroup;
+    public function __construct(
+        CustomerGroup $customerGroup
+    ) {
+        $this->_customerGroup = $customerGroup;
+    }
+
+    public function getCustomerGroup() {
+        $groups = $this->_customerGroup->toOptionArray();
+        return $groups;
+    }
+}
