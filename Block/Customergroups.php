826744042f8f1be38c4b476cf92fc02687226af0
walter <santi.walter@gmail.com>
2020-04-18 22:36:27 +0200

    Added admin configuration for groups to show in FE

--- Block/Customergroups.php
+++ Block/Customergroups.php
@@ -3,6 +3,12 @@
 namespace Ws\Customer\Block;
 
 
+use Ws\Customer\Helper\Data;
+
+/**
+ * Class Customergroups
+ * @package Ws\Customer\Block
+ */
 Class Customergroups extends \Magento\Framework\View\Element\Template {
 
     /**
@@ -13,6 +19,11 @@ Class Customergroups extends \Magento\Framework\View\Element\Template {
     protected $_customerGroup;
 
     /**
+     * @var Data
+     */
+    protected $_groups;
+
+    /**
      * @param \Magento\Backend\Block\Template\Context $context
      * @param \Magento\Customer\Model\ResourceModel\Group\Collection $customerGroup
      * @param array $data
@@ -20,19 +31,37 @@ Class Customergroups extends \Magento\Framework\View\Element\Template {
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Customer\Model\ResourceModel\Group\Collection $customerGroup,
-        array $data = []
+        array $data = [],
+        Data $groups
     ) {
         $this->_customerGroup = $customerGroup;
+        $this->_groups = $groups;
         parent::__construct($context, $data);
     }
+    
     /**
      * Get customer groups
      *
      * @return array
      */
-    public function getCustomerGroups() {
+    public function getCustomerGroups(): array {
         $customerGroups = $this->_customerGroup->toOptionArray();
-        array_unshift($customerGroups, array('value'=>'', 'label'=>'Any'));
+        foreach ($customerGroups as $index=>$value){
+            if(!in_array($value['value'], $this->getConfiguredGroups())){
+                unset($customerGroups[$index]);
+            }
+        }
+        //array_unshift($customerGroups, array('value'=>'', 'label'=>'Any'));
         return $customerGroups;
     }
+
+
+    /**
+     * @return array
+     */
+    protected function getConfiguredGroups(): array {
+        return explode(",",$this->_groups->getConfiguredCustomerGroups());
+    }
+
+
 }
---
+++ Helper/Data.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ws\Customer\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+    public function getConfig($path = '')
+    {
+        if ($path) return $this->scopeConfig->getValue($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return $this->scopeConfig;
+    }
+
+
+    public function getConfiguredCustomerGroups(){
+        return $this->getConfig('ws_customer/ws_form/ws_groups');
+    }
+
+}
+
