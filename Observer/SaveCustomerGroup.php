a5dde79a6de77bc527e5a171a909b7afe1a6f1d3
walter <santi.walter@gmail.com>
2020-04-19 20:22:16 +0200

    Added admin config

--- Observer/SaveCustomerGroup.php
+++ Observer/SaveCustomerGroup.php
@@ -21,14 +21,17 @@ class SaveCustomerGroup implements ObserverInterface
 
         $accountController = $observer->getAccountController();
         $request = $accountController->getRequest();
-        $group_id = preg_match("/[0-9]+/", $request->getParam('group_id')) ? $request->getParam('group_id'): self::CUSTOMER_GROUP_ID;
+        if($request->getParam('group_id') && preg_match("/[0-9]+/", $request->getParam('group_id'))){
+            $group_id = $request->getParam('group_id');
+        }else{
+            $group_id = self::CUSTOMER_GROUP_ID;
+        }
+        //$group_id = preg_match("/[0-9]+/", $request->getParam('group_id')) ? $request->getParam('group_id'): self::CUSTOMER_GROUP_ID;
 
         try{
-
             $customer = $observer->getEvent()->getCustomer();
             $customer->setGroupId($group_id);
             $this->_customerRepositoryInterface->save($customer);
-
         } catch(\Exception $e){
             echo $e->getMessage();
         }
